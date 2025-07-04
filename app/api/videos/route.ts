9b3d1e7f66db13f3fcd608343c23be61799f2f11
umang <umangkumar386@gmail.com>
2025-07-04 14:06:56 +0530

    Initial commit

---
+++ app/api/videos/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "@/generated/prisma";
+//import { PrismaClient } from "../../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
+  try {
+    const videos = await prisma.video.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching videos" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
